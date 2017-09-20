28da8c34fb498700f7c1502733132148be3eabda
vladuniqum <vladuniqum@gmail.com>
2017-09-20 20:02:49 +0300

    add training page

---
+++ js/mathpad.js
@@ -0,0 +1,68 @@
+ var fntext = "1";
+ function draw (){
+    functionPlot({
+        target: '#mathPlot',
+        xAxis: {
+          label: 'real'
+        }, 
+        yAxis: {
+          label: 'imaginary' 
+        },
+        grid: true,
+        data: [
+          { fn: fntext }
+        ]
+	})
+}
+
+draw();
+function updateFormula(){
+  $("#eq").val(fntext);
+}
+draw();
+
+$("#btnConst").on("click", function(){
+  fntext = "1";
+  updateFormula();
+  draw();
+});
+$("#btn3x").on("click", function(){
+  fntext = "3x";
+  updateFormula();
+  draw();
+});
+$("#btnAbs").on("click", function(){
+  fntext = "abs (x)";
+  updateFormula();
+  draw();
+});
+$("#btnSqr").on("click", function(){
+  fntext = "sqrt (x)";
+  updateFormula();
+  draw();
+});
+$("#btnX2").on("click", function(){
+  fntext = "x^2";
+  updateFormula();
+  draw();
+});
+$("#btnX3").on("click", function(){
+  fntext = "x^3";
+  updateFormula();
+  draw();
+});
+$("#btnOneByX").on("click", function(){
+  fntext = "1/x";
+  updateFormula();
+  draw();
+});
+$("#btnSin").on("click", function(){
+  fntext = "sin(x)";
+  updateFormula();
+  draw();
+});
+$("#btnGo").on("click", function(){
+  fntext = $("#eq").val();
+  draw();
+});
+
